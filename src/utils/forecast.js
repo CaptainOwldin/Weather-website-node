47826bbb93d3e7a31614584042491a1aebc5f144
CaptainOwldin <75150507+CaptainOwldin@users.noreply.github.com>
2020-12-06 01:29:22 +0100

    added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out!' + ' There is a ' + body.current.precip + '% chance of rain.') 
+            callback(undefined, 
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.  It feels like ' + body.current.feelslike + ' degrees out. There is a ' + body.current.precip + '% chance of rain. The humidity is ' + body.current.humidity + '%') 
 
         }
     })
